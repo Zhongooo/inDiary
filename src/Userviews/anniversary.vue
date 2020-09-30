d2c2e4363e3283c24a4e324c1d0850ae5ad1c5d3
Zhong7007 <azgungfvg@qq.com>
2020-09-30 19:20:32 +0800

    anniversary

--- src/Userviews/anniversary.vue
+++ src/Userviews/anniversary.vue
@@ -1,38 +1,38 @@
 <template>
-    <div style="background: whitesmoke;height:630px">
+    <div class="anniversary">
         <mt-header fixed title="纪念日">
             <router-link to="/user" slot="left">
                 <i class="iconfont icon-fanhui2" style="font-size:20px"></i>
             </router-link>
                     
         </mt-header>
-        <div style="position: relative;top:46px;background:#fff" v-for="item in list">
-            <div id="outout">
-                <div id="anniversaryPic">
-                    <img :src="require(`../assets/pic/anniversary/${item.icon}.png`)" id="anniversaryImg">
+        <div class="anniContent" v-for="item in list">
+            <div class="outout">
+                <div class="anniversaryPic">
+                    <img :src="require(`../assets/pic/anniversary/${item.icon}.png`)" class="anniversaryImg">
                 </div>
-                <div id="middlewords">
-                    <span style="font-size:18px;letter-spacing: 2px;">{{item.content}}</span><br />
-                    <span style="position: relative;top:8px;">
+                <div class="middlewords">
+                    <span class="annicontent">{{item.content}}</span><br />
+                    <span class="svgicon">
                         <svg class="iconcolor iconsvg" aria-hidden="true">
                             <use xlink:href="#icon-rili1"></use>
                         </svg>
-                        <span style="color:#828282">{{$moment(item.time).format('YYYY-MM-DD')}}</span>
+                        <span style="color:#666">{{$moment(item.time).format('YYYY-MM-DD')}}</span>
                     </span>
                 </div>
-                <div id="sumDay">
+                <div class="sumDay">
                     <span class="sumDayfont">已经</span><br />
-                    <div class="time"><span
-                            style="font-size:23px;">{{$moment().diff($moment(item.time), 'day')}}</span>&nbsp;<span>天</span>
+                    <div class="time">
+                        <span
+                            style="font-size:1rem;">{{$moment().diff($moment(item.time), 'day')}}</span>&nbsp;<span>天</span>
                     </div>
                 </div>
-                <div class="van-hairline--bottom" style="position: relative;top:80px;width:90%;left:5%;"></div>
             </div>
         </div>
 
-        <div id="addnew" @click="add">
-            <img src="../assets/pic/anniversary/add2.png" style="width:50px;height:50px;left:20px;position: relative;">
-            <span id="addwords">添加纪念日</span>
+        <div class="addnew" @click="add">
+            <img src="../assets/pic/anniversary/add2.png" class="addimg">
+            <span class="addwords">添加纪念日</span>
         </div>
 
     </div>
